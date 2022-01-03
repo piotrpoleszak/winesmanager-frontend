55dd5df4e282b890b305aab950c2bdd21887d691
Poleszak <poleszak@protonmail.com>
2022-01-03 17:41:24 +0100

    UI Modal Logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,14 +28,25 @@ export class AppComponent implements OnInit {
     );
   }
 
-  public onOpenModal(wine: Wine, mode: string): void {
+  public onOpenModal(wine: Wine | null, mode: string): void {
+    const container = document.getElementById('main-container');
     const button = document.createElement('button');
     button.type = 'button';
     button.style.display = 'none';
     button.setAttribute('data-toggle', 'modal');
 
     if(mode === 'add') {
-      button.setAttribute('data-target', 'modal');
+      button.setAttribute('data-target', '#addWineModal');
     }
+
+    if(mode === 'edit') {
+      button.setAttribute('data-target', '#editWineModal');
+    }
+
+    if(mode === 'delete') {
+      button.setAttribute('data-target', '#deleteWineModal');
+    }
+    container?.appendChild(button);
+    button.click();
   }
 }
